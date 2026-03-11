ddefef7a7a880d78a1aa82794df8e7c93769ccc3
Flambe02 <thepimentaorougecompany@gmail.com>
2026-03-11 18:30:29 -0300

    Hide static home fallback during JS hydration

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
   "version": "2.0.12",
-  "commit": "1f3e955",
-  "generatedAt": "2026-03-11T20:40:38.725Z",
-  "buildId": "2.0.12-1f3e955-2026-03-11T20-40-38-725Z"
+  "commit": "5e5b565",
+  "generatedAt": "2026-03-11T21:29:50.720Z",
+  "buildId": "2.0.12-5e5b565-2026-03-11T21-29-50-720Z"
 };
